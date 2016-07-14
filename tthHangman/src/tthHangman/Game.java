80ef0f08181b9488caca282d8c36162da060637c
Johnny Romano <john.p.romano@gmail.com>
2016-07-14 11:35:04 -0700

    Fixed formatting
    Correcting formatting issues in game and hangman classes

--- tthHangman/src/tthHangman/Game.java
+++ tthHangman/src/tthHangman/Game.java
@@ -1,31 +1,19 @@
 package tthHangman;
 
-public class Game {
-	  private String mAnswer;
-	  private String mHits;
-	  private String mMisses;
-	  
-	  public Game(String answer) {
-	    mAnswer = answer;
-	    mHits = "";
-	    mMisses = "";
-	  
-	  }
-	  public class Game {
-		  public static final int MAX_MISSES = 7;
-		  private String mAnswer;
-		  private String mHits;
-		  private String mMisses;
+	public class Game {
+		public static final int MAX_MISSES = 7;
+		private String mAnswer;
+		private String mHits;
+		private String mMisses;
 		  
-		  public Game(String answer) {
-		    mAnswer = answer;
+		public Game(String answer) {
+			mAnswer = answer;
 		    mHits = "";
 		    mMisses = "";
 		  
-		  }
-		  
-		  private char validateGuess(char letter) {
-		    if (! Character.isLetter(letter)) {
+		}
+		private char validateGuess(char letter) {
+			if (! Character.isLetter(letter)) {
 		      throw new IllegalArgumentException("A letter is required.");
 		    }
 		    letter = Character.toLowerCase(letter);
@@ -34,18 +22,15 @@ public class Game {
 		    }
 		    return letter;
 		    
-		  }
-		  
-		  public boolean applyGuess(String letters) {
+		}
+		public boolean applyGuess(String letters) {
 		   if (letters.length() == 0) {
 		     throw new IllegalArgumentException("No letter found");
 		   }
 		   return applyGuess(letters.charAt(0));
-		  }
-		  
-		  
-		  public boolean applyGuess(char letter) {
-		    letter = validateGuess(letter);
+		}
+		public boolean applyGuess(char letter) {
+			letter = validateGuess(letter);
 		    boolean isHit = mAnswer.indexOf(letter) >= 0;
 		    if (isHit) {
 		      mHits += letter;
@@ -53,10 +38,9 @@ public class Game {
 		      mMisses += letter; 
 		    }
 		    return isHit; 
-		  }
-		 
-		  public String getCurrentProgress() {
-		    String progress = "";
+		}
+		public String getCurrentProgress() {
+			String progress = "";
 		    for (char letter: mAnswer.toCharArray()) {
 		      char display = '-';
 		      if (mHits.indexOf(letter) >= 0) {
@@ -65,31 +49,15 @@ public class Game {
 		      progress += display;
 		    }
 		    return progress;
-		  }
-		  
-		  public int getRemainingTries() {
-		    return MAX_MISSES - mMisses.length();
-		  }
-		  
-		  public String getAnswer() {
-		    return mAnswer;
-		  }
-		  
-		  
-		  public boolean isSolved() {
-		    return getCurrentProgress().indexOf('-') == -1;
-		  }
-		  
-		  
 		}
-	  public boolean applyGuess(char letter) {
-	   boolean isHit = mAnswer.indexOf(letter) >= 0;
-	    if (isHit) {
-	      mHits += letter;
-	    } else {
-	      mMisses += letter; 
-	    }
-	    return isHit; 
-	  }
-	  
+		public int getRemainingTries() {
+		    return MAX_MISSES - mMisses.length();
+		}
+		public String getAnswer() {
+		    return mAnswer;
+		}
+		public boolean isSolved() {
+		    return getCurrentProgress().indexOf('-') == -1;
+		}
 	}
+	
--- tthHangman/src/tthHangman/Hangman.java
+++ tthHangman/src/tthHangman/Hangman.java
@@ -1,14 +1,12 @@
 package tthHangman;
 
 public class Hangman {
-    
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         // Enter amazing code here:
       if (args.length == 0) {
         System.out.println("Please enter a word..");
         System.exit(0);
       }
-      
       Game game = new Game(args[0]);
       Prompter prompter = new Prompter(game);
       prompter.play();
